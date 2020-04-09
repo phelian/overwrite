e985b07dc37ecbbbfa776f0db5fdc0591b741e50
Alexander Félix <alexander.felix@docly.com>
2020-04-09 22:17:59 +0200

    Simple types implemented
    * String
    * Int, Int8, Int16, Int32, Int64
    * Uint, Uint8, Uint16, Uint32, Uint64
    * Bool

--- do.go
+++ do.go
@@ -7,17 +7,60 @@ import (
 
 // Error types of package overwrite
 var (
-	ErrNotSameType  = errors.New("Dst and Src are not the same type")
-	ErrSrcNil       = errors.New("Input src type is nil")
-	ErrDstNil       = errors.New("Input dst type is nil")
-	ErrDstNotPtr    = errors.New("Input dst must be pointer")
-	ErrSrcNotStruct = errors.New("Input src must be struct")
+	ErrNotSameType    = errors.New("Dst and Src are not the same type")
+	ErrSrcNil         = errors.New("Input src type is nil")
+	ErrDstNil         = errors.New("Input dst type is nil")
+	ErrDstNotPtr      = errors.New("Input dst must be pointer")
+	ErrSrcNotStruct   = errors.New("Input src must be struct")
+	ErrCannotSetField = errors.New("Field not addressable and/or cannot be set")
+
+	tagOverwrite = "overwrite"
 )
 
 // Do write here
 func Do(dst, src interface{}) error {
 	if err := checkInput(dst, src); err != nil {
 		return err
+	}
+
+	t := reflect.TypeOf(src)
+	vDst := reflect.ValueOf(dst)
+	vSrc := reflect.ValueOf(src)
+
+	// Iterate over all available fields and read the tag value
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+
+		// Skip kinds that are not supported
+		if !checkSupportedKind(field.Type.Kind()) {
+			continue
+		}
+
+		// Get the field tag value
+		tag, err := newTags(field.Tag.Get(tagOverwrite))
+		if err != nil {
+			return err
+		}
+
+		// Ignore non tagged fields but let the struct types continue to be executed recursively
+		if !tag.overwrite && field.Type.Kind() != reflect.Struct {
+			continue
+		}
+
+		if err := checkCanSet(vDst.Elem(), field.Name); err != nil {
+			return err
+		}
+
+		if tag.omitempty && vSrc.Field(i).IsZero() {
+			continue
+		}
+
+		if field.Type.Kind() == reflect.Struct {
+			// TODO Handle
+		} else {
+			// Overwrite value
+			vDst.Elem().FieldByName(field.Name).Set(vSrc.Field(i))
+		}
 	}
 
 	return nil
@@ -44,5 +87,24 @@ func checkInput(dst, src interface{}) error {
 		return ErrNotSameType
 	}
 
+	return nil
+}
+
+func checkSupportedKind(kind reflect.Kind) bool {
+	switch kind {
+	case reflect.Struct, reflect.String, reflect.Bool,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return true
+	default:
+		return false
+	}
+}
+
+func checkCanSet(elem reflect.Value, name string) error {
+	value := elem.FieldByName(name)
+	if !(value.IsValid() && value.CanAddr() && value.CanSet()) {
+		return fmt.Errorf("Value (%v) Name (%s): %w", value, name, ErrCannotSetField)
+	}
 	return nil
 }
--- do_test.go
+++ do_test.go
@@ -34,5 +34,78 @@ func TestTypes(t *testing.T) {
 	err = Do(&T{}, T{})
 	require.Nil(t, err)
 }
+
+func TestSimpleTypes(t *testing.T) {
+	type T struct {
+		N   int    `overwrite:"true"`
+		N1  int8   `overwrite:"true"`
+		N2  int16  `overwrite:"true"`
+		N3  int32  `overwrite:"true"`
+		N4  int64  `overwrite:"true"`
+		UN  uint   `overwrite:"true"`
+		UN1 uint8  `overwrite:"true"`
+		UN2 uint16 `overwrite:"true"`
+		UN3 uint32 `overwrite:"true"`
+		UN4 uint64 `overwrite:"true"`
+		B   string `overwrite:"true"`
+		G   string `overwrite:"false"`
+		J   string
+		H   string `overwrite:"true,omitempty"`
+		L   bool   `overwrite:"true"`
+	}
+
+	tDst := &T{
+		N:   1,
+		N1:  11,
+		N2:  111,
+		N3:  1111,
+		N4:  11111,
+		UN:  1,
+		UN1: 11,
+		UN2: 111,
+		UN3: 1111,
+		UN4: 11111,
+		B:   "foo",
+		G:   "foop",
+		J:   "foob",
+		H:   "foot",
+		L:   false,
+	}
+
+	tSrc := T{
+		N:   42,
+		N1:  41,
+		N2:  4111,
+		N3:  41111,
+		N4:  411111,
+		UN:  31,
+		UN1: 31,
+		UN2: 3111,
+		UN3: 31111,
+		UN4: 311111,
+		B:   "bar",
+		G:   "",
+		J:   "",
+		H:   "",
+		L:   true,
+	}
+
+	err := Do(tDst, tSrc)
 	require.Nil(t, err)
+	require.Equal(t, tSrc.N, tDst.N)
+	require.Equal(t, tSrc.N1, tDst.N1)
+	require.Equal(t, tSrc.N2, tDst.N2)
+	require.Equal(t, tSrc.N3, tDst.N3)
+	require.Equal(t, tSrc.N4, tDst.N4)
+	require.Equal(t, tSrc.UN, tDst.UN)
+	require.Equal(t, tSrc.UN1, tDst.UN1)
+	require.Equal(t, tSrc.UN2, tDst.UN2)
+	require.Equal(t, tSrc.UN3, tDst.UN3)
+	require.Equal(t, tSrc.UN4, tDst.UN4)
+	require.Equal(t, tSrc.B, tDst.B)
+	require.NotEqual(t, tSrc.G, tDst.G)
+	require.NotEqual(t, tSrc.J, tDst.J)
+	require.NotEqual(t, tSrc.H, tDst.H)
+	require.Equal(t, tSrc.L, tDst.L)
+}
 }
